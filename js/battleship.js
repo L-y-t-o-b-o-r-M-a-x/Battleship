51227204aea75d37209d0aa7428d894c4cae8d94
Max Lagutin <L-u-t-o-b-o-r-M-a-x@yandex.ru>
2023-07-18 15:28:43 +0300

    Добавляет три метода в объект Модель: generateShipLocations, generateShip, collision

--- js/battleship.js
+++ js/battleship.js
@@ -14,8 +14,6 @@ let view = {
   },
 };
 
-// view.displayMessage("Tap tap, is this thing on?");
-
 // Объект Модель
 
 let model = {
@@ -25,50 +23,169 @@ let model = {
   shipsSunk: 0,
 
   ships: [
-    { locations: ["06", "16", "26"], hits: ["", "", ""] },
-    { locations: ["24", "34", "44"], hits: ["", "", ""] },
-    { locations: ["10", "11", "12"], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
   ],
 
   fire: function (guess) {
     for (let i = 0; i < this.numShips; i++) {
       let ship = this.ships[i];
       let index = ship.locations.indexOf(guess);
-      console.log(ship);
       if (index >= 0) {
         ship.hits[index] = "hit";
         view.displayHit(guess);
-        view.displayMessage("HIT!");
+        view.displayMessage("Есть пробитие!");
 
         if (this.isSunk(ship)) {
-          view.displayMessage("<< ЛИНКОР ПОТОПЛЕН!!! >>");
+          view.displayMessage("<< Линкор потоплен!!! >>");
           this.shipsSunk++;
         }
         return true;
       }
     }
     view.displayMiss(guess);
-    view.displayMessage("You missed.");
+    view.displayMessage("Вы промахнулись!");
     return false;
   },
 
-  isSunk: function(ship) {
+  isSunk: function (ship) {
     for (let i = 0; i < this.shipLength; i++) {
       if (ship.hits[i] !== "hit") {
         return false;
       }
     }
     return true;
+  },
+
+  generateShipLocations: function () {
+    let locations;
+    for (let i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+      this.ships[i].locations = locations;
+    }
+  },
+
+  generateShip: function () {
+    let direction = Math.floor(Math.random() * 2);
+    let row, col;
+
+    if (direction === 1) {
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+    } else {
+      row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+      col = Math.floor(Math.random() * this.boardSize);
+    }
+
+    let newShipLocations = [];
+    for (var i = 0; i < this.shipLength; i++) {
+      if (direction === 1) {
+        newShipLocations.push(row + "" + (col + i));
+      } else {
+        newShipLocations.push(row + i + "" + col);
+      }
+    }
+    return newShipLocations;
+  },
+
+  collision: function (locations) {
+    for (let i = 0; i < this.numShips; i++) {
+      let ship = model.ships[i];
+      for (let j = 0; j < locations.length; j++) {
+        if (ship.locations.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 };
 
-model.fire("53");
-model.fire("06");
-model.fire("16");
-model.fire("26");
-model.fire("34");
-model.fire("24");
-model.fire("44");
-model.fire("12");
-model.fire("11");
-model.fire("10");
+
+// Объект Контроллер
+
+let controller = {
+  guesses: 0,
+
+  processGuess: function (guess) {
+    let location = parseGuess(guess);
+    if (location) {
+      this.guesses++;
+      let hit = model.fire(location);
+
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage(
+          "Ты потопил все мои линкоры, в " + this.guesses + " выстрелов"
+        );
+      }
+    }
+  },
+};
+
+// Функция - обработка выстрела
+
+function parseGuess(guess) {
+  let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+  if (guess === null || guess.length !== 2) {
+    alert("«Ой, пожалуйста, введите букву и цифру на доске».");
+  } else {
+    firstChart = guess.charAt(0); // Метод charAt() возвращает указанный символ из строки.
+    let row = alphabet.indexOf(firstChart); // Метод indexOf() возвращает первый индекс, по которому данный элемент может быть найден в массиве или -1, если такого индекса нет.
+    let column = guess.charAt(1);
+
+    if (isNaN(row) || isNaN(column)) {
+      alert("«Ой, этого нет на доске».");
+    } else if (
+      row < 0 ||
+      row >= model.boardSize ||
+      column < 0 ||
+      column >= model.boardSize
+    ) {
+      alert("«Ой, это не по правилам!»");
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+}
+
+// Функция - обработчик события нажатия
+
+function handleFireButton() {
+  let guessInput = document.getElementById("guessInput");
+  let guess = guessInput.value;
+  controller.processGuess(guess);
+
+  guessInput.value = "";
+}
+
+//  Обработчик нажатий клавиш;
+
+function handleKeyPress(e) {
+  var fireButton = document.getElementById("fireButton");
+
+  e = e || window.event;
+
+  if (e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
+}
+
+window.onload = init;
+
+// Функция - обработчик
+
+function init() {
+  let fireButton = document.getElementById("fireButton");
+  fireButton.onclick = handleFireButton;
+
+  let guessInput = document.getElementById("guessInput");
+  guessInput.onkeypress = handleKeyPress;
+
+  model.generateShipLocations();
+}
